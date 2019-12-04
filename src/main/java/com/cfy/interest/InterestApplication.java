247b1cf1cbc7fd88a2ccead7cb08320584ac3641
chenfuyuan <chenfuyuan0713@163.com>
2019-12-04 12:28:38 +0800

    test git

---
+++ src/main/java/com/cfy/interest/InterestApplication.java
@@ -0,0 +1,13 @@
+package com.cfy.interest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cfy/interest/InterestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfy.interest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
